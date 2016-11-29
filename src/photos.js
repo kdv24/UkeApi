7d6513f29c04d41dbdcf352f79ddbaae41f7b09c
Kelly de Vries <kdv@me.com>
2016-11-28 16:00:47 -0800

    console log photos

--- src/photos.js
+++ src/photos.js
@@ -9,22 +9,22 @@ export default class Photos extends Component {
   }
 
 
-  function fetchDemo() {
+  fetchDemo() {
     fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=51a81174403dc17c24787f0cce329fef&format=json&nojsoncallback=1&tags=penguins').then(function(response) {
       return response.json();
     }).then(function(json) {
-      insertPhotos(json);
+      console.log(json.photos);
+      return json.photos;
     });
   }
 
 
-
   render() {
-
+    var fetchDemo = this.fetchDemo();
     return (
       <div>
         <p>hi</p>
-          fetchDemo();
+          <div><p>{fetchDemo}</p></div>
       </div>
     );
   }
