3d222a0ab2118d6159857a998b6b4dd5f63db2ed
Kelly de Vries <kdv@me.com>
2016-11-29 07:48:50 -0800

    add more detailed console log for json object

--- src/photos.js
+++ src/photos.js
@@ -8,23 +8,36 @@ export default class Photos extends Component {
     };
   }
 
+  componentWillMount() {
+    this.fetchDemo({
+        photos: this.props.photos,
+    })
+  }
+
 
   fetchDemo() {
+    let photos;
+    let photo_array;
+    // let single_photo;
     fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=51a81174403dc17c24787f0cce329fef&format=json&nojsoncallback=1&tags=penguins').then(function(response) {
       return response.json();
     }).then(function(json) {
-      console.log(json.photos);
-      return json.photos;
+      photos = json.photos;
+      photo_array  = json.photos.photo;
+      // single_photo = json.photos.photo;
+      console.log("these are the photos: ", photos);
+      console.log("this is an array of photos: ", photo_array);
+      console.log("this is a single photo: ", photo_array[0].id);
+      return photos;
     });
   }
 
 
   render() {
-    var fetchDemo = this.fetchDemo();
+    let all_the_marbles = this.photo_array;
     return (
       <div>
-        <p>hi</p>
-          <div><p>{fetchDemo}</p></div>
+          <div>{all_the_marbles}</div>
       </div>
     );
   }
