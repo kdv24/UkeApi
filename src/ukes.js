50537282f009d82a585062dc89e9d88f6ed1482b
Kelly de Vries <kdv@me.com>
2017-03-23 21:39:57 -0700

    comment out to troubleshoot

--- src/ukes.js
+++ src/ukes.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 // import style                from './style.js';
 // import $                    from 'jquery';
-import xml2js               from 'xml2js';
-import xml2json             from 'xml2json';
+// import xml2js               from 'xml2js';
+// import parser             from 'xml2json';
 
 
 export default class Ukes extends Component {
@@ -22,41 +22,43 @@ export default class Ukes extends Component {
   // }
 
 ///////////  JUST ADDED NEW NPM PACKAGE TO TRY  //////////////
-  var parser = require('xml2json');
 
-  var xml = "<foo attr=\"value\">bar</foo>";
-  console.log("input -> %s", xml)
-
-  // xml to json
-  var json = parser.toJson(xml);
-  console.log("to json -> %s", json);
-
-  // json to xml
-  var xml = parser.toXml(json);
-  console.log("back to xml -> %s", xml)
+  // let xml = "<foo attr=\"value\">bar</foo>";
+  // console.log("input -> %s", xml)
+  //
+  // // xml to json
+  // var json = parser.toJson(xml);
+  // console.log("to json -> %s", json);
+  //
+  // // json to xml
+  // var xml = parser.toXml(json);
+  // console.log("back to xml -> %s", xml)
 /////////////////////////////////////////////
 
   getChords(){
-    fetch('http://ukulele-chords.com/get?ak=d41d8cd98f00b204e9800998ecf8427e&r=D&typ=major', {mode: 'no-cors'})
+    fetch('http://localhost:4567/chords')
     .then(function(response) {
-      console.dir("response: ", response);
+      console.log("response: ", response);
 
-      let parseString = xml2js.parseString;
-      let xml = "<root>" + response + "</root>";
-      parseString(xml, function (err, result) {
-          console.log(result);
-
-          var chords = xml.getElementsByTagName("chord");
-          for(var i = 0; i < chords.length; i++) {
-              var chord = chord[i];
-              var chord_name = chord.getElementsByTagName("chord_name");
-              for(var j = 0; j < chord_name.length; j++) {
-                  alert(chord_name[j].childNodes[0].nodeValue);
-              }
-          }
-        });
-        console.log(xml);
+      // let parseString = xml2js.parseString;
+      // let xml = "<root>" + response + "</root>";
+      // parseString(xml, function (err, result) {
+      //     console.log(result);
+      //
+      //     var chords = xml.getElementsByTagName("chord");
+      //     for(var i = 0; i < chords.length; i++) {
+      //         var chord = chord[i];
+      //         var chord_name = chord.getElementsByTagName("chord_name");
+      //         for(var j = 0; j < chord_name.length; j++) {
+      //             alert(chord_name[j].childNodes[0].nodeValue);
+      //         }
+      //     }
+      //   });
+        // console.log(xml);
         // return this.parseXML(response);
+    })
+    .then(function(ukeData) {
+      console.log("ukeData", ukeData);
     });
   }
 
